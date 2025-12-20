a979031975cfd5abb1d029246dc58f028b2bb210
Manish <manishkumar9540898@gmail.com>
2025-12-20 19:28:49 +0530

    chore(ui):setup accentricity add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import Hero from "@/components/hero";
+
 const Home = () => (
-  <main className="h-screen w-screen black-gradient">portfolio</main>
+  <main className="bg-black-100 relative mx-auto flex flex-col items-center justify-center overflow-hidden sm:px-10">
+    <div className="w-full max-w-7xl">
+      <Hero />
+    </div>
+  </main>
 );
+
 export default Home;
---
+++ components/hero.tsx
@@ -0,0 +1,62 @@
+import { FaLocationArrow } from "react-icons/fa6";
+
+import MagicButton from "./MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
+const Hero = () => {
+  return (
+    <div className="pt-36 pb-20">
+      {/**
+       *  UI: Spotlights
+       *  Link: https://ui.aceternity.com/components/spotlight
+       */}
+      <div>
+        <Spotlight className="-top-40 -left-10 h-screen md:-top-20 md:-left-32" fill="white" />
+        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      {/**
+       *  UI: grid
+       *  change bg color to bg-black-100 and reduce grid color from
+       *  0.2 to 0.03
+       */}
+      <div className="dark:bg-black-100 dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] absolute top-0 left-0 flex h-screen w-full items-center justify-center bg-white">
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+          className="dark:bg-black-100 pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="relative z-10 my-20 flex justify-center">
+        <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+          <p className="max-w-80 text-center text-xs tracking-widest text-blue-100 uppercase">
+            Dynamic Web Magic with Next.js
+          </p>
+
+          {/**
+           *  Link: https://ui.aceternity.com/components/text-generate-effect
+           *
+           *  change md:text-6xl, add more responsive code
+           */}
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="mb-4 text-center text-sm md:text-lg md:tracking-wider lg:text-2xl">
+            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+          </p>
+
+          <a href="#about">
+            <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
